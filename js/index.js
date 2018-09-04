2d2a94eb427bd2d57e0d28d42c517f4d1bb7d677
Jose Vega Sainz <javegasainz@gmail.com>
2018-09-03 23:13:31 -0300

    version final

--- js/index.js
+++ js/index.js
@@ -1,15 +1,21 @@
+var init = prompt('Elija una opción: 1. Crear una nueva clase, o 2. Solicitar listado de una clase.');
 
-function getLocalList(input) {
-
-    var studentsArray = input.split(' ');
-
-    localStorage.setItem('sList', JSON.stringify(studentsArray));
-    
-    var storedList = localStorage.getItem('sList');
-
+if (init === '1') {
+    function setLS(key, value) {
+    return localStorage.setItem(key, JSON.stringify(value));
+    }
+    var className = prompt('Elija el nombre de su clase.');
+    var inputNames = prompt('Ingrese el nombre de cada estudiante separado por un espacio');
+    var inputList = inputNames.split(' ');
+    setLS(className, inputList);
+} else if (init === '2') {
+    function returnClass(className) {
+    var storedList = localStorage.getItem(className);
     var returnedList = JSON.parse(storedList);
-
-    return console.log(returnedList);
+    return alert(returnedList);
+    }
+    var solClass = prompt('Ingrese el nombre de su Clase.');
+    returnClass(solClass);
+} else {
+    alert('Error, por favor recargue la página a inicie de nuevo.')
 }
-
-getLocalList(prompt('Ingrese el nombre de cada estudiante separado por un espacio.'));
